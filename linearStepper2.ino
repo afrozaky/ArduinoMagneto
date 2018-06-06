6cb74505ba8848d26fac090f362c8d01e59b388f
pdeo6 <39602869+pdeo6@users.noreply.github.com>
2018-06-06 14:12:18 -0400

    Update linearStepper2.ino

--- linearStepper2.ino
+++ linearStepper2.ino
@@ -1,15 +1,23 @@
-const double startFreq = 300;      //in Hz, min value of 62 Hz by precision of delayMicroseconds
-const double maxFreq = 6000;      //in Hz, max value value of 330 kHz precision of delayMicroseconds
-const double finalPeriod = 1 / (maxFreq) * pow(10, 6);                  //Final period in microseconds
-const double basePeriod = 1 / (startFreq) * pow(10, 6);                 //base period or the period to ramp up from and ramp down to
+/** LINEAR STEPPER ACCELERATION
+ *  
+ *  Authors        : Zaky Hussein, Priyanka Deo
+ *  Lasst Modified : June 6th, 2018
+ *  
+ *  The following program linearly accelerates a stepper motor to bring it to a maximum frequency, runs it at the frequency for a set time, 
+ *  then decelerates it back to a minimum frequency. Then it changes the direction of rotation and repeats the sequence.
+ */
+
+// INITIALIZING
+const double startFreq = 250;                                           // [Hz] min value of 62 Hz by precision of delayMicroseconds
+const double maxFreq = 3000;                                            // [Hz] max value of 330 kHz by precision of delayMicroseconds
+const double finalPeriod = 1 / (maxFreq) * pow(10, 6);                  // [us] final period corresponding to maximum frequency
+const double basePeriod = 1 / (startFreq) * pow(10, 6);                 // [us] base period or the period to ramp up from and ramp down to
 double period = basePeriod;
-unsigned long initialTime;
-const double ratio = 0.1;                                          //ratio of rampTime / total time
-const double totalTime = 1;                              //total time to ramp up, maintain max frequency, and ramp down
-const double peakTime = (1 - ratio) * totalTime;                                 //number of seconds for the time max frequency should be maintained
+unsigned long initialTime; 
+const double peakTime;                                                  // [ms] time for which max frequency should be maintained
 const double rampTime = 0.5 * ratio * totalTime;            //Time for one individual ramp up or down
-unsigned int df = 50;                       //Change in freq (Hz)
-unsigned int dt = 3000;                     //Change in time (us)
+const double df = 135;                       //Change in freq (Hz)
+unsigned int dt = 2950;                     //Change in time (us)
 double pulseAcceleration  = df / dt;      
 const byte DIR = 9;
 const byte STEP = 10;
@@ -26,7 +34,7 @@ void setup() {
 
 void loop() {
   //Initial Direction
-  //digitalWrite(DIR, !digitalRead(DIR));
+  digitalWrite(DIR, !digitalRead(DIR));
   initialTime2 = millis();
 
   //Ramp up to max frequency
@@ -40,9 +48,11 @@ void loop() {
     }
     freq+=df;
 //    freq = 1/period * pow(10,6);
-    Serial.println(freq);
+//    Serial.println(freq);
     period = 1/freq * pow(10,6);    
   }
+//  Serial.print(millis()-initialTime2);
+//  Serial.print(" ");
 
   //Remain at max frequency
   initialTime = millis();
@@ -52,10 +62,13 @@ void loop() {
     digitalWrite(STEP, LOW);
     delayMicroseconds(period / 2.0);
 //    freq = 1/finalPeriod* pow(10,6);
-    Serial.println(freq);
+//    Serial.println(freq);
   }
+//   Serial.print(millis()-initialTime);
+//   Serial.print(" ");
 
   //Ramp down from max frequency
+  initialTime2 = millis();
   while (period < basePeriod) {
     initialTime = micros();
     while(micros() - initialTime < dt){
@@ -66,10 +79,9 @@ void loop() {
     }
     freq-=df;
 //    freq = 1/period * pow(10,6);
-    Serial.println(freq);
+//    Serial.println(freq);
     period = 1/freq * pow(10,6); 
   }
 //    Serial.println(millis() - initialTime2);
 
 }
-
