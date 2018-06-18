14b9d23256d4376e7f105fffac2320929a5877df
pdeo6 <39602869+pdeo6@users.noreply.github.com>
2018-06-18 10:10:02 -0400

    Update stepControlUserInput.ino

--- stepControlUserInput.ino
+++ stepControlUserInput.ino
@@ -1,45 +1,43 @@
 /** LINEAR STEPPER ACCELERATION
     Authors       : Zaky Hussein, Priyanka Deo
-    Last Modified : June 6th, 2018
+    Last Modified : June 18th, 2018
     The following program linearly accelerates a stepper motor to bring it to a maximum frequency, runs it at the frequency for a set time,
     then decelerates it back to a minimum frequency. Then it changes the direction of rotation and repeats the sequence.
 */
 
 // INPUTS
-double strokeLength = 0;                                                     // [mm] stroke length to cover in a half cycle
-double totalTime = 0;                                                         // [s] time for one half cycle/full stroke length
+double strokeLength = 0;                                                            // [mm] stroke length to cover in a half cycle
+double totalTime = 0;                                                               // [s] time for one half cycle/full stroke length
+double cycles = 0;
 
 // DECLARING CONSTANTS
-const double peakTime = sqrt(pow(totalTime, 2) - 3840 * strokeLength / pow(10, 6)); // [s] time for which max frequency should be maintained
+double peakTime;
 const double startFreq = 250;                                                       // [Hz] min value of 62 Hz by precision of delayMicroseconds
-const double maxFreq = pow(10, 6) * (totalTime - peakTime) / 48;
-const double finalPeriod = 1 / (maxFreq) * pow(10, 6);                              // [us] final period corresponding to maximum frequency
-const double basePeriod = 1 / (startFreq) * pow(10, 6);                             // [us] base period or the period to ramp up from and ramp down to
+double maxFreq;
+double finalPeriod;
+double basePeriod;
 const double df = 135;                                                              // [Hz] frequency increments
 unsigned int dt = 2950;                                                             // [us] time increments
 const byte DIR = 9;                                                                 // direction pin on Arduino board
 const byte STEP = 10;                                                               // clock pin on Arduino board
-const double pulseAcceleration  = df / dt * pow(10, 6);                             // [s^-2] frequency acceleration rate
-const double nPulses = ((maxFreq - startFreq) / 2.0) * .024 * maxFreq * .001;
-const double constPulses = maxFreq * peakTime;
+double nPulses;
+double constPulses;
 int sensor;
-int cycles = 2;
 int counter = 0;
 
-
 // NON CONSTANT VARIABLES
-double period = basePeriod;
 unsigned long initialTime;
 double freq = startFreq;
 unsigned long stepCount = 0;
 unsigned long initialTime2;
+double period;
 
 // SETUP
 void setup() {
   pinMode(DIR, OUTPUT);
   pinMode(STEP, OUTPUT);
   pinMode(A0, INPUT);
-  digitalWrite(DIR, HIGH);   //LOW -> TOWARDS MOTOR
+  digitalWrite(DIR, LOW);   //LOW -> TOWARDS MOTOR; HIGH -> AWAY FROM MOTOR
   digitalWrite(STEP, LOW);
   Serial.begin(2000000);
   //  Serial.print(peakTime);
@@ -47,44 +45,54 @@ void setup() {
   //  Serial.print(nPulses);
   //  Serial.print(" ");
   //  Serial.print(maxFreq);
-  sensor = analogRead(A0);
   //  Serial.print(" ");
+  sensor = analogRead(A0);
 }
 
 void loop() {
-  //Handling user input section
-  if (totalTime == 0 || strokeLength == 0) {
-    counter = 0;                                              //Reset counter
-    Serial.println("Enter total time in seconds > 0.3: ");      //Prompt User for input
+
+  //HANDLING USER INPUT SECTION
+  if (totalTime == 0 || strokeLength == 0 || cycles == 0) {
+    counter = 0;                                                               //Reset counter
+    Serial.println("Enter total time in seconds : ");                          //Prompt User for input
     while (Serial.available() == 0) {}
-    totalTime = Serial.parseFloat();              //Read user input into totalTime
+    totalTime = Serial.parseFloat();                                           //Read user input into totalTime
 
     if (totalTime < 0.3) {
       totalTime = 0;
       Serial.println("Do not enter a total time < 0.3 seconds");
     } else {
-      Serial.println("Enter stroke length in mm: ");      //Prompt User for input
+      Serial.println("Enter stroke length in mm: ");                           //Prompt User for input
       while (Serial.available() == 0) {}
-      strokeLength = Serial.parseFloat();              //Read user input into height
+      strokeLength = Serial.parseFloat();                                      //Read user input into height
+      Serial.println("Enter number of cycles: ");                           //Prompt User for input
+      while (Serial.available() == 0) {}
+      cycles = Serial.parseFloat();                                      //Read user input into height
 
       if (totalTime != 0 && strokeLength != 0) {
         Serial.print("Total time entered: ");
         Serial.println(totalTime);
         Serial.print("Stroke length entered: ");
         Serial.println(strokeLength);
+        Serial.print("Number of cycles entered: ");
+        Serial.println(strokeLength);
         Serial.println("Beginning motor control sequence");
+        peakTime = sqrt(pow(totalTime, 2) - 3840 * strokeLength / pow(10, 6)); // [s] time for which max frequency should be maintained
+        maxFreq = pow(10, 6) * (totalTime - peakTime) / 48;
+        finalPeriod = 1 / (maxFreq) * pow(10, 6);                              // [us] final period corresponding to maximum frequency
+        basePeriod = 1 / (startFreq) * pow(10, 6);                             // [us] base period or the period to ramp up from and ramp down to
+        nPulses = ((maxFreq - startFreq) / 2.0) * .024 * maxFreq * .001;
+        constPulses = maxFreq * peakTime;
+        period = basePeriod;
       }
     }
+
+    //MAIN MOTOR CONTROL SECTION OF LOOP
   } else if (counter < 2 * cycles) {
-    //MAIN MOTOR CONTROL SECTION of LOOP
-
     stepCount = 0;
-    Serial.println(analogRead(A0));
-
-    //DIRECTION
-    digitalWrite(DIR, !digitalRead(DIR));
-
     initialTime2 = millis();
+    sensor = analogRead(A0);
+    Serial.println(sensor);
 
     // RAMP UP TO MAX FREQUENCY
     while (stepCount < nPulses && freq < maxFreq) {
@@ -101,34 +109,18 @@ void loop() {
       freq += df;
       period = 1 / freq * pow(10, 6);
     }
-    //  Serial.print(millis() - initialTime2);
-    //  Serial.print(" ");
-    //  Serial.print((double)stepCount / 40.0);
-    //  Serial.print(" ");
-    //  Serial.println(freq);
 
     // REMAIN AT MAX FREQUENCY
     stepCount = 0;
-    initialTime = millis();
     for (int i = 0; i < constPulses; i++) {
       digitalWrite(STEP, HIGH);
       delayMicroseconds(period / 2.0);
       digitalWrite(STEP, LOW);
       delayMicroseconds(period / 2.0);
-      // freq = 1 / finalPeriod * pow(10,6);
-      Serial.println(freq);
       stepCount++;
-
     }
 
-    //  Serial.print(millis() - initialTime);
-    //  Serial.print(" ");
-    //  Serial.print((double)stepCount / 40.0);
-    //  Serial.print(" ");
-    //  Serial.println(freq);
-
     // RAMP DOWN FROM MAX FREQUENCY
-    //  initialTime2 = millis();
     stepCount = 0;
     while (freq > startFreq)  {
       initialTime = micros();
@@ -138,26 +130,19 @@ void loop() {
         digitalWrite(STEP, LOW);
         delayMicroseconds(period / 2.0);
         stepCount++;
-        //       if(stepCount>=nPulses)
-        //        break;
       }
       freq -= df;
-      // freq = 1 / period * pow(10,6);
-      // Serial.println(freq);
       period = 1 / freq * pow(10, 6);
     }
 
     sensor = analogRead(A0);
-    //  Serial.print(millis() - initialTime2);
-    //  Serial.print(" ");
-    //  Serial.print((double)stepCount / 40.0);
-    //  Serial.print(" ");
-    //  Serial.println(freq);
-    //Serial.print(" ");
     Serial.println(millis() - initialTime2);
+    Serial.println(stepCount);
     Serial.println(sensor);
-    counter++;                              //End of one half cycle, increment counter
-    delay(0.1 * totalTime * pow(10, 3));         //Add delay between changing directions to match Gauss curve
+    counter++;                                        //End of one half cycle, increment counter
+    delay(0.1 * totalTime * pow(10, 3));              //Add delay between changing directions to match Gauss curve
+
+    //DIRECTION SWITCH
+    digitalWrite(DIR, !digitalRead(DIR));
   }
 }
-
