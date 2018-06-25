eddd88ef38d1bca7ff527feeb13c543be90aa874
pdeo6 <39602869+pdeo6@users.noreply.github.com>
2018-06-25 16:05:23 -0400

    Update stepControlUserInput.ino

--- stepControlUserInput.ino
+++ stepControlUserInput.ino
@@ -1,9 +1,15 @@
-/** LINEAR STEPPER ACCELERATION
+/** LINEAR STEPPER MOTOR ACCELERATION
     Authors       : Zaky Hussein, Priyanka Deo
     Last Modified : June 18th, 2018
     The following program linearly accelerates a stepper motor to bring it to a maximum frequency, runs it at the frequency for a set time,
-    then decelerates it back to a minimum frequency. Then it changes the direction of rotation and repeats the sequence.
-*/
+    then decelerates it back to a minimum frequency. Then it changes the direction of rotation and repeats the sequence a given number of times.
+    Please follow the folllowing instructions for running the code:
+    1) Hit the upload button
+    2) Turn the motor driver ON
+    3) Select Tools -> Serial Monitor
+    4) Follow instructions on the serial monitor
+    Please DO NOT touch the following script, thank you.    
+**/
 
 // INPUTS
 double strokeLength = 0;                                                            // [mm] stroke length to cover in a half cycle
@@ -19,7 +25,8 @@ double basePeriod;
 const double df = 135;                                                              // [Hz] frequency increments
 unsigned int dt = 2950;                                                             // [us] time increments
 const byte DIR = 9;                                                                 // direction pin on Arduino board
-const byte STEP = 10;                                                               // clock pin on Arduino board
+const byte CLOCK = 10;                                                              // clock pin on Arduino board
+const byte LIMIT = 7;                                                               // limit switch
 double nPulses;
 double constPulses;
 int sensor;
@@ -35,10 +42,9 @@ double period;
 // SETUP
 void setup() {
   pinMode(DIR, OUTPUT);
-  pinMode(STEP, OUTPUT);
+  pinMode(CLOCK, OUTPUT);
   pinMode(A0, INPUT);
-  digitalWrite(DIR, LOW);   //LOW -> TOWARDS MOTOR; HIGH -> AWAY FROM MOTOR
-  digitalWrite(STEP, LOW);
+  digitalWrite(CLOCK, LOW);
   Serial.begin(2000000);
   //  Serial.print(peakTime);
   //  Serial.print(" ");
@@ -47,68 +53,82 @@ void setup() {
   //  Serial.print(maxFreq);
   //  Serial.print(" ");
   sensor = analogRead(A0);
+  homeleft();
+  digitalWrite(DIR, HIGH);   //LOW -> TOWARDS MOTOR; HIGH -> AWAY FROM MOTOR
+}
+
+// HOME TO THE LEFT LIMIT SWITCH
+void homeleft() {
+  digitalWrite(DIR, LOW);    //LOW  -> TOWARDS MOTOR (LEFT)
+  double homingFreq = 750;
+  double homingPeriod = 1 / homingFreq * pow(10, 6);
+  while (digitalRead(LIMIT) == HIGH) {   //Run at homing frequency until limit hit
+    digitalWrite(CLOCK, HIGH);
+    delayMicroseconds(homingPeriod / 2.0);
+    digitalWrite(CLOCK, LOW);
+    delayMicroseconds(homingPeriod / 2.0);
+  }
 }
 
 void loop() {
 
-  //HANDLING USER INPUT SECTION
+  // HANDLING USER INPUT SECTION
   if (totalTime == 0 || strokeLength == 0 || cycles == 0) {
-    counter = 0;                                                               //Reset counter
-    Serial.println("Enter total time in seconds : ");                          //Prompt User for input
+    counter = 0;                                                             // Reset counter
+    Serial.println("Enter total time in seconds ");                          // Prompt User for input
     while (Serial.available() == 0) {}
-    totalTime = Serial.parseFloat();                                           //Read user input into totalTime
+    totalTime = Serial.parseFloat();                                         // Read user input into totalTime
 
-    if (totalTime < 0.3) {
-      totalTime = 0;
-      Serial.println("Do not enter a total time < 0.3 seconds");
-    } else {
-      Serial.println("Enter stroke length in mm: ");                           //Prompt User for input
-      while (Serial.available() == 0) {}
-      strokeLength = Serial.parseFloat();                                      //Read user input into height
-      Serial.println("Enter number of cycles: ");                           //Prompt User for input
-      while (Serial.available() == 0) {}
-      cycles = Serial.parseFloat();                                      //Read user input into height
+    //    if (totalTime < 0.3) {
+    //      totalTime = 0;
+    //      Serial.println("Do not enter a total time < 0.3 seconds");
+    //    } else {
+    Serial.println("Enter stroke length in mm ");                            //Prompt User for input
+    while (Serial.available() == 0) {}
+    strokeLength = Serial.parseFloat();                                      //Read user input into height
+    Serial.println("Enter number of cycles ");                               //Prompt User for input
+    while (Serial.available() == 0) {}
+    cycles = Serial.parseFloat();                                            //Read user input into height
 
- if (totalTime != 0 && strokeLength != 0 && cycles != 0) {
-        Serial.print("Total time entered: ");
-        Serial.println(totalTime);
-        Serial.print("Stroke length entered: ");
-        Serial.println(strokeLength);
-        Serial.print("Number of cycles entered: ");
-        Serial.println(cycles);
-        Serial.print("Stroke Frequency: ");
-        Serial.println(1.0 / (2.0 * totalTime));
+    if (totalTime != 0 && strokeLength != 0) {
+      Serial.print("Total time entered: ");
+      Serial.println(totalTime);
+      Serial.print("Stroke length entered: ");
+      Serial.println(strokeLength);
+      Serial.print("Number of cycles entered: ");
+      Serial.println(cycles);
+      Serial.print("Stroke Frequency: ");
+      Serial.println(1.0 / (2.0 * totalTime));
 
-        //Initialize variables from user input
-        peakTime = sqrt(pow(totalTime, 2) - 3840 * strokeLength / pow(10, 6)); // [s] time for which max frequency should be maintained
-        maxFreq = pow(10, 6) * (totalTime - peakTime) / 48;
-        finalPeriod = 1 / (maxFreq) * pow(10, 6);                              // [us] final period corresponding to maximum frequency
-        basePeriod = 1 / (startFreq) * pow(10, 6);                             // [us] base period or the period to ramp up from and ramp down to
-        nPulses = ((maxFreq - startFreq) / 2.0) * .024 * maxFreq * .001;
-        constPulses = maxFreq * peakTime;
-        period = basePeriod;
+      //Initialize variables from user input
+      peakTime = sqrt(pow(totalTime, 2) - 3840 * strokeLength / pow(10, 6)); // [s] time for which max frequency should be maintained
+      maxFreq = pow(10, 6) * (totalTime - peakTime) / 48;
+      finalPeriod = 1 / (maxFreq) * pow(10, 6);                              // [us] final period corresponding to maximum frequency
+      basePeriod = 1 / (startFreq) * pow(10, 6);                             // [us] base period or the period to ramp up from and ramp down to
+      nPulses = ((maxFreq - startFreq) / 2.0) * .024 * maxFreq * .001;
+      constPulses = maxFreq * peakTime;
+      period = basePeriod;
 
-        Serial.print("Max RPM: ");
-        Serial.println(maxFreq / 200.0 * 60);
-        Serial.println("Beginning motor control sequence in 3 seconds");
-        delay(3000);                          //Give user time to read out
-      }
+      Serial.print("Max RPM: ");
+      Serial.println(maxFreq / 200.0 * 60);
+      Serial.println("Beginning motor control sequence in 3 seconds");
+      delay(3000);                                                           // Give user time to read out
     }
 
-    //MAIN MOTOR CONTROL SECTION OF LOOP
+    // MAIN MOTOR CONTROL SECTION OF LOOP
   } else if (counter < 2 * cycles) {
     stepCount = 0;
     initialTime2 = millis();
     sensor = analogRead(A0);
-    Serial.println(sensor);
+    //    Serial.println(sensor);
 
     // RAMP UP TO MAX FREQUENCY
     while (stepCount < nPulses && freq < maxFreq) {
       initialTime = micros();
       while (micros() - initialTime < dt) {
-        digitalWrite(STEP, HIGH);
+        digitalWrite(CLOCK, HIGH);
         delayMicroseconds(period / 2.0);
-        digitalWrite(STEP, LOW);
+        digitalWrite(CLOCK, LOW);
         delayMicroseconds(period / 2.0);
         stepCount++;
         if (stepCount >= nPulses)
@@ -121,9 +141,9 @@ void loop() {
     // REMAIN AT MAX FREQUENCY
     stepCount = 0;
     for (int i = 0; i < constPulses; i++) {
-      digitalWrite(STEP, HIGH);
+      digitalWrite(CLOCK, HIGH);
       delayMicroseconds(period / 2.0);
-      digitalWrite(STEP, LOW);
+      digitalWrite(CLOCK, LOW);
       delayMicroseconds(period / 2.0);
       stepCount++;
     }
@@ -133,9 +153,9 @@ void loop() {
     while (freq > startFreq)  {
       initialTime = micros();
       while (micros() - initialTime < dt) {
-        digitalWrite(STEP, HIGH);
+        digitalWrite(CLOCK, HIGH);
         delayMicroseconds(period / 2.0);
-        digitalWrite(STEP, LOW);
+        digitalWrite(CLOCK, LOW);
         delayMicroseconds(period / 2.0);
         stepCount++;
       }
@@ -144,13 +164,13 @@ void loop() {
     }
 
     sensor = analogRead(A0);
-    Serial.println(millis() - initialTime2);
-    Serial.println(stepCount);
-    Serial.println(sensor);
-    counter++;                                        //End of one half cycle, increment counter
-    delay(0.1 * totalTime * pow(10, 3));              //Add delay between changing directions to match Gauss curve
+    //    Serial.println(millis() - initialTime2);
+    //    Serial.println(stepCount);
+    //    Serial.println(sensor);
+    counter++;                                        // End of one half cycle, increment counter
+    delay(0.1 * totalTime * pow(10, 3));              // Add delay between changing directions to match Gauss curve
 
-    //DIRECTION SWITCH
+    // DIRECTION SWITCH
     digitalWrite(DIR, !digitalRead(DIR));
   }
 }
