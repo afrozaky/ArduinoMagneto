499d90d510458dfc582614ae28edb0c4a66201dd
pdeo6 <39602869+pdeo6@users.noreply.github.com>
2018-06-26 10:26:05 -0400

    Add files via upload

---
+++ integration/testWaitTime/testWaitTime.ino
@@ -0,0 +1,26 @@
+#define interruptPin 3
+int IRread = 0;
+int lastIRread = 0;
+
+void setup() {
+  // Initialize the serial port and display
+  Serial.begin(2000000);
+
+  // Enable the pullup resistor and attach the interrupt
+  pinMode(interruptPin, INPUT_PULLUP);
+}
+
+void waitTime(){
+  while (IRread != HIGH && IRread == lastIRread){   
+    lastIRread = IRread;  
+    delay(10);
+    IRread = digitalRead(interruptPin);
+  }
+  Serial.print("New cycle begin at ");
+  Serial.println(millis());
+}
+
+void loop() {
+  waitTime();
+}
+
